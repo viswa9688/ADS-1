3dfae4ac17616663fc8287b15c1354ebe9d35339
viswa9688 <55698248+viswa9688@users.noreply.github.com>
2019-11-11 16:39:44 +0530

    Solution.java

--- Day 2/Josepheus/Josephus/Solution.java
+++ Day 2/Josepheus/Josephus/Solution.java
@@ -1,4 +1,32 @@
 // The time complexity is n^2.
+/*
+
+    Integer[] array1=new Integer[]{-30,20,10,20,30,-50,80,-20,-10};
+    int current;    
+    public void threeS()
+    {
+        Arrays.sort(array1);
+        int j;
+        int k;
+        for(int i=0;i<array1.length;i++)
+        {
+            
+            //System.out.println(i);
+            for(j=0;j<array1.length-1;j++)
+            {
+                //System.out.println(j);
+                
+                k=j+1;
+                //System.out.println(k);
+                //System.out.println(array1[i]+" "+array1[j]+" "+array1[k]);
+                
+                if((array1[i]+(array1[j]+array1[k]))==0){
+                    System.out.println(array1[i]+"i "+array1[j]+"j "+array1[k]);
+                }
+
+
+
+*/
 
 import java.util.*;
 class Solution{
