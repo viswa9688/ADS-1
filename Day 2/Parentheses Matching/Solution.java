c1e55f893cfd8cb4120a17c4dbfe26ae266c0fd3
viswa9688 <55698248+viswa9688@users.noreply.github.com>
2019-11-11 16:41:17 +0530

    Solution.java

--- Day 2/Parentheses Matching/Solution.java
+++ Day 2/Parentheses Matching/Solution.java
@@ -1,4 +1,34 @@
 class Solution {
+	/*
+	 int top=-1; 
+        char items[] = new char[100]; 
+  
+        void push(char x)  
+        { 
+            if (top == 99)  
+            { 
+                System.out.println("Stack full"); 
+            }  
+            else 
+            { 
+                items[++top] = x; 
+            } 
+        } 
+  
+        char pop()  
+        { 
+            if (top == -1)  
+            { 
+                System.out.println("Underflow error"); 
+                return '\0'; 
+            }  
+            else 
+            { 
+                char element = items[top]; 
+                top--; 
+                return element; 
+            } 
+	*/
 	static class stack {
 		private int top = -1;
 		private char[] items = new char[100];
