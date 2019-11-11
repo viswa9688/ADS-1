8bd899081424e7c94b2706259d8f5ef12d68bf5a
viswa9688 <55698248+viswa9688@users.noreply.github.com>
2019-11-11 16:45:05 +0530

    Solution.java

--- Da4/quick sort/Solution.java
+++ Da4/quick sort/Solution.java
@@ -55,25 +55,7 @@ class Solution{
             k++; 
         } 
     } 
-	public static int[] quickSort(int[] arr){
-		// StdRandom.shuffle(arr);
-		sort(arr, 0, arr.length - 1);
-		return arr;
-	}
-	private static void exch(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-	private static void sort(int[] arr, int lo, int hi) { 
-        if (hi <= lo) return;
-        int j = partition(arr, lo, hi);
-        sort(arr, lo, j-1);
-        sort(arr, j+1, hi);
-	}
-	private static int partition(int[] arr, int lo, int hi) {
-        int i = lo;
-        int j = hi + 1;
+	
         while (true) {            
             while (arr[++i] < arr[lo]) {
                 if (i == hi) break;
